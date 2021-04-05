6206fdee41406fe6fcbb702198bc6dc5b46d993f
Diana Liao <dijali@gmail.com>
2021-04-05 09:14:58 -0400

    commit tree fbe0142d55cdbb7476d6432850a16d954480d534

--- index.js
+++ index.js
@@ -10,4 +10,36 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+  let timeInfo = time.split(":");
+  let hour = parseInt(timeInfo[0]);
+  let minute = parseInt(timeInfo[1]);
+  
+  // if (hour < 12){
+  //   return "Good Morning";
+  // } else if (hour > 17){
+  //   return "Good Evening";
+  // } else {
+  //   return "Good Afternoon";
+  // }
+
+  // why doesn't this work?
+  let message;
+  if (hour < 12){
+    message = "Good Morning";
+  } else if (hour > 17){
+    message = "Good Evening";
+  } else {
+    message = "Good Afternoon";
+  }
+
+  return message;
+
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg;
+}
